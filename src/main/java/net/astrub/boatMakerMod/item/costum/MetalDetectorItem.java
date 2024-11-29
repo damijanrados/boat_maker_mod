01c27344fa6cf342afb7fa8b5bbf23a8f59dc59c
damijanrados <33632425+damijanrados@users.noreply.github.com>
2024-11-29 22:54:42 +0100

    adding more ores, with droppable material

---
+++ src/main/java/net/astrub/boatMakerMod/item/costum/MetalDetectorItem.java
@@ -0,0 +1,61 @@
+package net.astrub.boatMakerMod.item.costum;
+
+import net.minecraft.client.resources.language.I18n;
+import net.minecraft.core.BlockPos;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+
+import java.util.Objects;
+
+public class MetalDetectorItem extends Item{
+
+    public MetalDetectorItem(Properties pProperties) {
+        super(pProperties);
+    }
+
+    @Override
+    public InteractionResult useOn(UseOnContext pContext) {
+        //checks all the blocks below the player and returns if any of them are ores
+        if (pContext.getLevel().isClientSide()){
+            BlockPos positionClicked = pContext.getClickedPos();
+            Player player = pContext.getPlayer();
+            boolean foundBlock = false;
+
+            for (int i = 0; i<= positionClicked.getY() + 64; i++){
+                BlockState state = pContext.getLevel().getBlockState(positionClicked.below(i));
+
+                if(isValuableBlock(state)){
+                    assert player != null;
+                    outputValuableCoordinates(positionClicked.below(i), player, state.getBlock());
+                    foundBlock =true;
+
+                    break;
+                }
+            }
+            if (!foundBlock){
+                assert player != null;
+                player.sendSystemMessage(Component.literal("No valuables found"));
+            }
+        }
+
+        pContext.getItemInHand().hurtAndBreak(1, Objects.requireNonNull(pContext.getPlayer()),
+                player -> player.broadcastBreakEvent((player.getUsedItemHand())));
+
+        return InteractionResult.SUCCESS;
+    }
+
+    private void outputValuableCoordinates(BlockPos blockPosition, Player player, Block block) {
+        player.sendSystemMessage(Component.literal("Found " + I18n.get(block.getDescriptionId()) + " at " +
+                "(" + blockPosition.getX() + ", " + blockPosition.getY() + "," + blockPosition.getZ() + ")"));
+    }
+
+    private boolean isValuableBlock(BlockState state) {
+        return state.is(Blocks.IRON_ORE) || state.is(Blocks.DIAMOND_ORE);
+    }
+}
